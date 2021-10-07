8f0e3dfdbe56adcde8a978b0839a88cd22136fd4
Aleksey-devback <bolshoy.enot@mail.ru>
2021-10-08 04:22:19 +0800

    long_polling

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LongPulling from "./LongPulling.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <LongPulling />
+    </div>
+  );
+}
+
+export default App;
